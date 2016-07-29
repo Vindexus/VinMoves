1dc5a093f57360f90c50e011ae933b45016d68b5
unknown <vindexus@gmail.com>
2016-07-29 16:13:00 -0700

    Copycaster next version.

--- moves/copycaster.js
+++ moves/copycaster.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "Copycaster",
-  description: '<p>When you are <strong>the first member of your party to cast a spell where this foe can sense it</strong>, they gain magical ability based on your skills. Name two spells you have access to (even if they aren\'t currently prepared) and roll+WIS. On a 10+ you pick which one of those two spells they can cast. On a 7-9 the GM will pick. On a 6- they can cast both.</p>',
+  description: '<p>When you are <strong>the first spell-casting member of your party to use magic where this foe can sense it</strong>, they gain the ability to cast a spell from your class, roll+WIS. On a 10+, choose any spell from your class that they get access to. On a 7-9 the same, but the spell must be of your level or greater. On a 6-the GM will choose a spell from your class of any level.</p>',
   releaseDate: '2016-08-01',
   author: 'Colin "Vindexus" Kierans',
   authorUrl: 'http://vindexus.net',
-  flavor: ""
+  flavor: "Your powers betray you."
 };
