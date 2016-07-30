3968897f1f40bdce3e5ac0e2b067d4853f0a39b5
Colin Kierans <vindexus@gmail.com>
2016-07-29 17:53:39 -0700

    Formatting.

--- moves/copycaster.js
+++ moves/copycaster.js
@@ -1,6 +1,8 @@
 module.exports = {
   name: "Copycaster",
-  description: '<p>When you are <strong>the first spell-casting member of your party to use magic where this foe can sense it</strong>, they gain the ability to cast a spell from your class, roll+WIS. On a 10+, the GM will choose a level 1 spell from your class. On a 7-9 the same, but the spell must be of your level or greater. On a 6-the GM will choose a spell from your class of any level.</p>',
+  description: '<p>When you are <strong>the first spell-casting member of your party to use magic where this foe can sense it</strong>, ' +
+  'they gain the ability to cast a spell from your class, roll+WIS. On a 10+, the GM will choose a level 1 spell from your class. ' +
+  'On a 7-9 the same, but the spell must be of your level or greater. On a 6-the GM will choose a spell from your class of any level.</p>',
   releaseDate: '2016-08-01',
   author: 'Colin "Vindexus" Kierans',
   authorUrl: 'http://vindexus.net',
