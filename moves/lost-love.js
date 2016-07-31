9f8437f8415ef1ad9f6f2f38a7631f356e474123
unknown <vindexus@gmail.com>
2016-07-31 14:29:30 -0700

    Updated to reflect blog post text.

--- moves/lost-love.js
+++ moves/lost-love.js
@@ -3,13 +3,16 @@ module.exports = {
   description: '<p>When you are <strong>the first party member to make physical contact with this foe</strong>, you get a vision of something terrible that happened to a ' +
   'loved one in their past, roll+CHA. On a 10+ choose two, on a 7-9 choose one:</p>' +
   '<ul>' +
-    '<li>You get a clear sense of that person, describe them.</li>'+
+    '<li>You get a clear sense of that person, the GM will tell you their name and describe them</li>'+
     '<li>You know what happened to them, describe it</li>'+
-    '<li>You know who was responsible, the GM will tell you who</li>'+
+    '<li>You know who or what was responsible, the GM will tell you</li>'+
   '</ul>'+
-  '<p>Take +1 forward when acting on the information.</p>',
+  '<p>On a 7+, take +1 forward when acting on the information.</p>',
   flavor: "Tis better to have lost and avenged than to never have loved at all.",
   releaseDate: '2016-08-03',
   author: 'Vindexus',
-  authorUrl: 'http://vindexus.net'
+  authorUrl: 'http://vindexus.net',
+  urls: {
+    blogPost: 'http://vindexus.net/2016/07/31/lost-love-vin-moves/'
+  }
 }
