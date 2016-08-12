d32518cc364b2e22cf1ed0aea8d201939ac1ede8
unknown <vindexus@gmail.com>
2016-08-11 22:07:44 -0700

    Moves to md

---
+++ moves/famous-weapon.md
@@ -0,0 +1,15 @@
+# Famous Weapon
+When this **enemy brandishes their notorious weapon to the group**, the party member with the highest Intelligence score recognizes it and rolls+INT. On a 7+ choose two and describe them:
+
+ - It's said to leave lasting wounds beyond cuts and bruises
+ - It crackles or shimmers with an elemental force
+ - It is a sentient weapon with a mind of its own
+ - It is holy (or corrupt)
+ - It is a long lost weapon of a famous order, guild, or family
+ - It was constructed using a rare and powerful material'
+
+On a 10+ the GM will tell you what tags, bonuses, and additional effects the weapon has.
+
+---
+author: Vindexus  
+authorUrl: http://vindexus.net  
--- moves/sanguine-binding.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "Sanguine Binding",
-  description: '<p>This enemy has the power to use its blood to force a link between it and you. The <strong>first time this enemy’s blood touches you</strong> it binds its life force to yours, roll+CHA. On a 7+ whenever it takes damage, you take the same amount ignoring armor and describe how this damage manifests itself.<br />' +
-    ' On a 10+ the link works both ways; when you take damage so does this enemy.<br />' +
-    'On a 6- this link affects not just you – pick a party member you have a Bond with to be affected in the same way.</p>' +
-  '<p>This effect lasts about a day unless the enemy is killed or you find some way to break the link.</p>',
-  author: 'Colin "Vindexus" Kierans',
-  authorUrl: 'http://vindexus.net',
-}
