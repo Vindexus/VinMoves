0e9e5d0b45ef6fe8fc104ebabb0618d6e5c62af5
unknown <vindexus@gmail.com>
2016-08-11 22:01:50 -0700

    Two new moves in js format.

---
+++ moves/famous-weapon.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "Famous Weapon",
+  description: '<p>When this <strong>enemy brandishes their notorious weapon to the group</strong>, the party member with the highest Intelligence score recognizes it and rolls+INT. On a 7+ choose two and describe them:</p>' +
+  '<ul><li>' +
+    (["It's said to leave lasting wounds beyond cuts and bruises",
+      'It crackles or shimmers with an elemental force',
+      'It is a sentient weapon with a mind of its own',
+      'It is holy (or corrupt)',
+      'It is a long lost weapon of a famous order, guild, or family',
+      'It was constructed using a rare and powerful material'].join("</li><li>")) +
+    '</li></ul>' +
+  '<p>On a 10+ the GM will tell you what tags, bonuses, and additional effects the weapon has.</p>',
+  author: 'Colin "Vindexus" Kierans',
+  authorUrl: 'http://vindexus.net',
+}
+
--- moves/venom-attack.js
+++ moves/venom-attack.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'Venom Attack',
   description: '<p>When you <strong>take damage after armor from this foe for the first time</strong>, the venom seeps into your veins, roll+CON. ' + 
-  'On a 10+ you fight off the effects of the venom. On a 7-9 you aren\'t so luck, choose one. On a 6- the GM will also choose one:</p>' +
+  'On a 10+ you fight off the effects of the venom. On a 7-9 you aren\'t so lucky, choose one. On a 6- the GM will also choose one:</p>' +
   '<ul>' +
     '<li>It saps the strength from you, take -1d6 damage forward</li>'+
     '<li>It burns in your body, take 1d6 damage ignoring armor</li>'+
